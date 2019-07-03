d81b03913a81d22b8c86c92a6eb822929a7c3411
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-03 04:55:05 -0400

    inti database

---
+++ server/knexfile.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const localPg = {
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_DATABASE
+};
+
+const pg = require('pg');
+
+const dbConnection = process.env.DATABASE_URL || localPg;
+
+module.exports = {
+	development: {
+		client: 'pg',
+		connection: dbConnection,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	},
+
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
+
+	production: {
+		client: 'pg',
+		connection: dbConnection,
+		migrations: {
+			directory: './database/migrations'
+		},
+		seeds: {
+			directory: './database/seeds'
+		}
+	}
+};
