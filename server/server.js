528a0c13ccc51f0af326ff2ebf1e478a902884cd
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-03 04:30:40 -0400

    built out serverjs

--- server/server.js
+++ server/server.js
@@ -1,2 +1,33 @@
 const express = require("express");
+const server = express();
 
+//controllers here
+
+var randomMessage = [
+    "Hello World",
+    "Welcome Developer",
+    "Sometimes you just got to roll the dice",
+    "Welcome to the happiest server on this side of the sea, love.",
+    "Reminder: Tip Your Server",
+    "Don't Worry 🐝  Happy",
+    ];
+var message = randomMessage[Math.floor(Math.random() * randomMessage.length)];
+
+    server.get("/", (req, res) => {
+    res.status(200).json({ message });
+    });
+
+    let now = new Date().getTime();
+
+    if (require.main == module) {
+    server.listen(process.env.PORT, () => {
+        console.log(
+        `*\n
+        ${message}\n
+        🚀  Server is running at http://localhost:${process.env.PORT}/ \n
+        TIME IS: ${now}`
+        );
+    });
+    } else {
+    module.exports = server;
+    }
