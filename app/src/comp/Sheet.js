cc24f38381795bf9f0f26ee858053e3aedaf79fe
Taylor <wwwaaavvveeerrr@gmail.com>
2019-07-09 23:35:06 -0400

    backend and frontend connected

--- app/src/comp/Sheet.js
+++ app/src/comp/Sheet.js
@@ -1,10 +1,22 @@
-import React, { Component } from 'react'
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import "../sass/sheet.sass"
 export default class Sheet extends Component {
+    state = {
+
+    }
+    componentDidMount(){
+        const endpoint = 'http://localhost:5000/example';
+        axios
+        .get(endpoint)
+        .then(res=>{
+        this.setState({ex: [...res.data.message]});
+}).catch(err => console.log(err));
+    }
     render() {
         return (
-            <div>
-                vses
+            <div className="charCard">
+                {this.state.ex}
             </div>
         )
     }
--- server/router/sheet.js
+++ server/router/sheet.js
@@ -4,12 +4,12 @@ const db = require("./dbfuntions")
 router.get("/", async (req, res) => {
     try {
         const cSheet = await db.findExample()
-        const message = "The charater sheets were found in the database.";
+        const message = "The character sheets were found in the database.";
         res.status(200).json({ message, cSheet });
     } catch (error) {
         res.status(500).json({
         message:
-            "Sorry but something went wrong while retrieving the list of charater sheets"
+            "Sorry but something went wrong while retrieving the list of character sheets"
         });
 
         throw new Error(error);
